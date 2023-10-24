2574849d1a4b6fb22fd015b26299e31f8594fc55
lmontiel92 <146214091+lmontiel92@users.noreply.github.com>
2023-10-24 19:08:43 +0000

    intial commit and rock page functionality

--- app.rb
+++ app.rb
@@ -7,3 +7,38 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if comp_move == "rock"
+    outcome = "We tied!"
+
+  end
+
+  if comp_move == "paper"
+    outcome = "We lost!"
+
+  end
+
+  if comp_move == "scissors"
+    outcome = "We won!"
+
+  end
+"
+<h2>
+  We played rock!
+</h2>
+
+<h2>
+ They played #{comp_move}!
+</h2>
+
+<h2>
+  #{outcome}
+</h2>
+"
+end
